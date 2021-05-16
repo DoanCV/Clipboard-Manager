bddf1fecca5a29d99267614dd6e95fa6ddc85ab8
Brian <briandoan07@gmail.com>
2021-05-16 12:40:03 -0400

    Added connection to MongoDB with index.js
    Database name: snippet

--- Clipboard-Manager/server/index.js
+++ Clipboard-Manager/server/index.js
@@ -9,10 +9,22 @@ app.use(express.json());
 
 app.listen(5000, () => console.log("Server started on port 5000"));
 
-
 // set up router
-
 app.use("/snippet", require("./routers/snippetRouter"));
+
+// connect to MongoDB
+    // // mongodb+srv://devistry:<password>@clipboardmanager.oxqfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    // databse name: snippet
+    // replace password with your own
+mongoose.connect("mongodb+srv://devistry:<password>@clipboardmanager.oxqfy.mongodb.net/snippet?retryWrites=true&w=majority", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+});
+
+
 
 /*
 app.get("/test", (req, res) => {
