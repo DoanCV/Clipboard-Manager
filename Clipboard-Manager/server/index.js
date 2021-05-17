6bba8db1c01aef14e5e9bf401af35f336a4fef50
Brian <briandoan07@gmail.com>
2021-05-17 00:35:55 -0400

    Moved password to .env

--- Clipboard-Manager/server/index.js
+++ Clipboard-Manager/server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// load in MongoDB database password
+dotenv.config();
 
 // set up an express server
-
 const app = express();
 
 app.use(express.json());
@@ -14,13 +17,15 @@ app.use("/snippet", require("./routers/snippetRouter"));
 
 // connect to MongoDB
     // mongodb+srv://devistry:<password>@clipboardmanager.oxqfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-    // databse name: snippet
+    // database name: snippet
     // replace password with your own
-mongoose.connect("mongodb+srv://devistry:<password>@clipboardmanager.oxqfy.mongodb.net/snippet?retryWrites=true&w=majority", { 
+mongoose.connect(process.env.MDB_CONNECT_STRING, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }, (err) => {
-    if (err) return console.error(err);
+    if (err) {
+        return console.error(err);
+    }
     console.log("Connected to MongoDB");
 });
 
