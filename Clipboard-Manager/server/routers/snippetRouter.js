b2a5df71865d059a849024fae68d72bddbf59624
Brian <briandoan07@gmail.com>
2021-05-17 00:02:52 -0400

    Implemented CRUD backend endpoints

--- Clipboard-Manager/server/routers/snippetRouter.js
+++ Clipboard-Manager/server/routers/snippetRouter.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
 const Snippet = require("../models/snippetModel");
 
-router.post("/", async (req, res) => {
+
+// Read clip endpoint
+router.get("/", async (req, res) => {
+
     try {
+
+        const snippets = await Snippet.find();
+        res.json(snippets);
+
+    }
+    catch(err) {
+
+        // in case of mongoose error
+            // empty send for security
+        res.status(500).send();
+
+    }
+
+});
+
+
+// Create clip endpoint
+router.post("/", async (req, res) => {
+
+    try {
+
         // const body = req.body;
         // console.log(body);
 
@@ -22,14 +46,56 @@ router.post("/", async (req, res) => {
         const savedSnippet = await newSnippet.save();
 
         res.json(savedSnippet);
+
     } 
     catch(err) {
+
         // in case of mongoose error
+            // empty send for security
         res.status(500).send();
+
     }
+
+});
+
+
+// Delete clip endpoint
+router.delete("/:id", async (req, res) => {
+
+    try {
+
+        const snippetID = req.params.id;
+        // console.log(snippetID);
+
+        // validation for missing ID
+        if (!snippetID){
+            return res.status(400).json( {errorMessage: "Contact developer with this query: Missing snippetID."} );
+        }
+
+        const existingSnippet = await Snippet.findById(snippetID);
+        if (!existingSnippet){
+            return res.status(400).json( {errorMessage: "Contact developer with this query: Missing clip with given snippetID"} );
+        }
+
+        // await Snippet.findByIdAndDelete();
+            // redundant so the line below is faster
+        await existingSnippet.delete();
+
+        res.json(existingSnippet);
+
+    }
+    catch(err){
+
+        // in case of mongoose error
+            // empty send for security
+        res.status(500).send();
+
+    }
+    
 });
 
 /*
+// Test endpoint
 router.get("/test", (req, res) => {
     res.send("Router test");
 });
