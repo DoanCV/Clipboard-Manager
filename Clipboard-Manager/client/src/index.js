d60fd31b5b3cb644a63154140198f834fe889bb2
Brian <briandoan07@gmail.com>
2021-05-18 11:52:37 -0400

    Test index.js input

--- Clipboard-Manager/client/src/index.js
+++ Clipboard-Manager/client/src/index.js
@@ -8,9 +8,16 @@ function clock() {
   <div>
     <h1>Clock</h1>
     <p>It is {new Date().toLocaleTimeString()}</p>
+    <input />
   </div>
   );
   ReactDOM.render(e1, document.getElementById("root"));
 }
 
 setInterval(clock, 1000);
+
+function message(){
+  return <h1>Chata</h1>;
+}
+
+ReactDOM.render(message, document.getElementById("root"));
