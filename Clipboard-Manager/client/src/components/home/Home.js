680767a9a01f5c1fcc07f40e3cec383cc3600d6e
Brian <briandoan07@gmail.com>
2021-05-19 12:41:33 -0400

    Properly added home components, need to render
    - Installed axios and cors

--- Clipboard-Manager/client/src/components/home/Home.js
+++ Clipboard-Manager/client/src/components/home/Home.js
@@ -1,6 +1,19 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+
+    const [snippets, setSnippets] = useState([]);
+
+    useEffect(() => {
+        getSnippets;
+    }, []);
+
+    async function getSnippets() {
+        const snippetsRes = await Axios.get("http://localhost:5000/snippet/");
+        setSnippets(snippetsRes.data);
+    }
+
     return <div className = "home">Home</div>
 }
 
--- Clipboard-Manager/server/index.js
+++ Clipboard-Manager/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors =  require("cors");
 
 // load in MongoDB database password
 dotenv.config();
@@ -9,6 +10,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+// middleware for access control origin headers (for frontend)
+app.use(
+    cors({
+        origin: "http://localhost:3000", 
+    })
+);
 
 app.listen(5000, () => console.log("Server started on port 5000"));
 
