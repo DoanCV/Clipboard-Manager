0d7994bea41bd83df0f8c64dfbd245ae8e5841ae
Brian <briandoan07@gmail.com>
2021-05-21 19:14:01 -0400

    Refactored editor to its own class to keep Home.js logic simple

--- Clipboard-Manager/client/src/components/home/Home.js
+++ Clipboard-Manager/client/src/components/home/Home.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 import Snippet from "./Snippet";
+import SnippetEditor from "./SnippetEditor";
 
 function Home() {
 
@@ -10,11 +11,6 @@ function Home() {
     // bool to check editor state, not open by default
     const [newSnippetEditorOpen, setNewSnippetEditorOpen] = useState(false);
 
-    // store editor state values
-    const [editorTitle, setEditorTitle] = useState("");
-    const [editorDescription, setEditorDescription] = useState("");
-    const [editorCode, setEditorCode] = useState("");
-
     useEffect(() => {
         getSnippets();
     }, []);
@@ -22,29 +18,6 @@ function Home() {
     async function getSnippets() {
         const snippetsRes = await Axios.get("http://localhost:5000/snippet/");
         setSnippets(snippetsRes.data);
-    }
-
-    async function saveClipboard(e) {
-        e.preventDefault();
-
-        const snippetData = {
-            title: editorTitle ? editorTitle : undefined,
-            description: editorDescription ? editorDescription : undefined,
-            code: editorCode ? editorCode : undefined
-        };
-
-        await Axios.post("http://localhost:5000/snippet/", snippetData);
-
-        getSnippets();
-        closeEditor();
-    }
-
-    // users may not want every clipboard to save, they must be able to cancel
-    function closeEditor() {
-        setNewSnippetEditorOpen(false);
-        setEditorTitle("");
-        setEditorDescription("");
-        setEditorCode("");
     }
 
     // iterate through an array with information set after a response from axios
@@ -72,39 +45,10 @@ function Home() {
                 </button>
             )}
             {newSnippetEditorOpen && (
-                <div className = "snippet-editor">
-                    <form onSubmit = {saveClipboard}>
-                        <label htmlFor = "editor-title">Title</label>
-                        <input 
-                            id = "editor-title" 
-                            type = "text" 
-                            value = {editorTitle} 
-                            onChange = {(e) => setEditorTitle(e.target.value)}
-                        />
-
-                        <label htmlFor = "editor-description">Description</label>
-                        <input 
-                            id = "editor-description" 
-                            type = "text" 
-                            value = {editorDescription}
-                            onChange = {(e) => setEditorDescription(e.target.value)}
-                        />
-
-                        <label htmlFor = "editor-code">Code</label>
-                        <textarea 
-                            id = "editor-code"
-                            value = {editorCode}
-                            onChange = {(e) => setEditorCode(e.target.value)}
-                        />
-
-                        {/* save the snippet */}
-                        <button type = "submit">Save clipboard</button>
-
-                        {/* cancel the snippet editor */}
-                        <button type = "button" onClick = {closeEditor}>Cancel</button>
-
-                    </form>
-                </div>
+                <SnippetEditor 
+                    setNewSnippetEditorOpen = {setNewSnippetEditorOpen} 
+                    getSnippets = {getSnippets}
+                />
             )}
             {renderSnippets()}
         </div>
---
+++ Clipboard-Manager/client/src/components/home/SnippetEditor.js
@@ -0,0 +1,71 @@
+import React, {useState} from "react";
+import Axios from "axios";
+
+function SnippetEditor(props) {
+
+    // store editor state values
+    const [editorTitle, setEditorTitle] = useState("");
+    const [editorDescription, setEditorDescription] = useState("");
+    const [editorCode, setEditorCode] = useState("");
+
+    async function saveClipboard(e) {
+        e.preventDefault();
+
+        const snippetData = {
+            title: editorTitle ? editorTitle : undefined,
+            description: editorDescription ? editorDescription : undefined,
+            code: editorCode ? editorCode : undefined
+        };
+
+        await Axios.post("http://localhost:5000/snippet/", snippetData);
+
+        props.getSnippets();
+        closeEditor();
+    }
+
+    // users may not want every clipboard to save, they must be able to cancel
+    function closeEditor() {
+        props.setNewSnippetEditorOpen(false);
+        setEditorTitle("");
+        setEditorDescription("");
+        setEditorCode("");
+    }
+
+    return (
+        <div className = "snippet-editor">
+            <form onSubmit = {saveClipboard}>
+                <label htmlFor = "editor-title">Title</label>
+                <input 
+                    id = "editor-title" 
+                    type = "text" 
+                    value = {editorTitle} 
+                    onChange = {(e) => setEditorTitle(e.target.value)}
+                />
+
+                <label htmlFor = "editor-description">Description</label>
+                <input 
+                    id = "editor-description" 
+                    type = "text" 
+                    value = {editorDescription}
+                    onChange = {(e) => setEditorDescription(e.target.value)}
+                />
+
+                <label htmlFor = "editor-code">Code</label>
+                <textarea 
+                    id = "editor-code"
+                    value = {editorCode}
+                    onChange = {(e) => setEditorCode(e.target.value)}
+                />
+
+                {/* save the snippet */}
+                <button type = "submit">Save clipboard</button>
+
+                {/* cancel the snippet editor */}
+                <button type = "button" onClick = {closeEditor}>Cancel</button>
+
+            </form>
+        </div>
+    );
+}
+
+export default SnippetEditor;
