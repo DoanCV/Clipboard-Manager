f6a5f7aefdb3bd2ce7b4550458949aea7143bef8
Brian <briandoan07@gmail.com>
2021-05-23 17:32:28 -0400

    Finished validating user account creation, applied bcrypt algorithm on password

--- Clipboard-Manager/server/routers/userRouter.js
+++ Clipboard-Manager/server/routers/userRouter.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 router.post("/", async (req, res) => {
     try {
+
         const { email, password, passwordConfirm } = req.body;
 
         // validate body
-        
+
         if (!email || !password || !passwordConfirm) {
             return res.status(400).json({
                 errorMessage: "You are missing required fields. Fill them in."
@@ -24,6 +27,29 @@ router.post("/", async (req, res) => {
                 errorMessage: "Your passwords do not match."
             });
         }
+
+        // check that no other account uses the provided email
+        const existingUser = await User.findOne({email: email});
+        if (existingUser) {
+            return res.status(400).json({
+                errorMessage: "An account with this email already exists."
+            });
+        }
+
+        // hash the password so users cannot read database password
+            // bcrypt algorithm
+                // apply random string to password then hashes that
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // add valid user to database
+
+        const newUser = new User({
+            email,
+            passwordHash
+        });
+
+        const savedUser = await newUser.save();
 
     }
     catch (err) {
